22bde4e1b0523eae4970be6fa07fa6613ab8f94c
Andrew <syn.kolbasyn.06@gmail.com>
2023-04-06 15:36:03 +0300

    Small settings up

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,38 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-int main(void)
-{
-    GLFWwindow* window;
+using std::cout;
+using std::endl;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
+int main(int argc, char** argv) {
+    if (!glfwInit()) {
+        cout << "glfwInit() - failed" << endl;
         return -1;
     }
 
-    /* Make the window's context current */
+    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL-game", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        cout << "glfwCreateWindow() - failed" << endl;
+        return -1;
+    }
+
     glfwMakeContextCurrent(window);
     
     if (!gladLoadGL()) {
+        cout << "gladLoadGL() - failed" << endl;
         return -1;
     }
     
     glClearColor(0, 1, 0, 1);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
